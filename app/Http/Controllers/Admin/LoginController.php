ea4a1769eacf0e3474dda616eb6e6e2a60281041
abcd1234 <iiooiio@hotmail.com>
2019-06-19 21:31:29 +0800

    update

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Crypt;
-
+use App\Http\Model\User;
 use Illuminate\Http\Request;
 
 require_once __DIR__ . '/../../../../resources/org/code/code.php';
@@ -19,7 +19,20 @@ class LoginController extends CommonController
             if(strtoupper($input['code'])!=strtoupper($_code)){
                 return back()->with('msg', '驗證碼錯誤');
             }
+            
+            $user = User::first();
+            
+            if($user->user_name != $input['user_name'] || 
+                    Crypt::decrypt($user->user_pass) != $input['user_pass'] ){
+                return back()->with('msg', '用戶名或密碼錯誤');
+            }
+            
+            session(['user'=>$user]);
+            echo "OK";
+            
         }else{
+            
+            
             return view('admin.login');
         }
         
@@ -28,6 +41,13 @@ class LoginController extends CommonController
     public function code(){
         $code = new \Code();
         $code->imgcode(165,34);
+    }
+    
+    public function crypt(){
+        $str = '123456';
+        
+        echo $crypt = Crypt::encrypt($str);
+        echo Crypt::decrypt($crypt);
     }
     
 }
